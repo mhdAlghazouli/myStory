eedd720a1b4a42879a555843550ee88f5f9a7ed4
mhdAlghazouli <94564835+mhdAlghazouli@users.noreply.github.com>
2022-03-31 14:04:06 -0400

    css

---
+++ myStory.js
@@ -0,0 +1,27 @@
+
+
+function fn1(){
+    if(document.getElementById("my-name").style.display === "block"){
+        document.getElementById("my-name").style.display = "none";
+    }else{
+        document.getElementById("my-name").style.display = "block";
+    }
+}
+
+
+function fn2(){
+    if(document.getElementById("main-img").style.display === "block"){
+        document.getElementById("main-img").style.display = "none";
+    }else{
+        document.getElementById("main-img").style.display = "block";
+    }
+}
+
+
+function fn3(){
+    if(document.getElementById("story-div").style.display === "block"){
+        document.getElementById("story-div").style.display = "none";
+    }else{
+        document.getElementById("story-div").style.display = "block"
+    }
+}
