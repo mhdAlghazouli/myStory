151c668ea32d1e6c5d025acaa5aeed72aa771f71
mhdAlghazouli <94564835+mhdAlghazouli@users.noreply.github.com>
2022-04-11 17:11:20 -0400

    change all the content

--- myStory.js
+++ myStory.js
@@ -1,27 +1,19 @@
+$(function () {
 
+'use strict';
 
-function fn1(){
-    if(document.getElementById("my-name").style.display === "block"){
-        document.getElementById("my-name").style.display = "none";
-    }else{
-        document.getElementById("my-name").style.display = "block";
-    }
-}
+// Adjust Header Height
+let myHeader = $('.header');
 
+myHeader.height($(window).height());
 
-function fn2(){
-    if(document.getElementById("main-img").style.display === "block"){
-        document.getElementById("main-img").style.display = "none";
-    }else{
-        document.getElementById("main-img").style.display = "block";
-    }
-}
+$(window).resize(function () {
+    myHeader.height($(window).height());
 
+});
+// links active
+$('.links li').click(function() {
+    $(this).addClass('active').siblings().removeClass('active');
+});
 
-function fn3(){
-    if(document.getElementById("story-div").style.display === "block"){
-        document.getElementById("story-div").style.display = "none";
-    }else{
-        document.getElementById("story-div").style.display = "block"
-    }
-}
+});
