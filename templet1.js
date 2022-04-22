06a23e3e67e998d1422c1c70190934f4c00e3cec
mhdAlghazouli <94564835+mhdAlghazouli@users.noreply.github.com>
2022-04-22 15:38:28 -0400

    make a js slider

--- templet1.js
+++ templet1.js
@@ -1,29 +1,53 @@
+
+// make a slider
+   //calling the BTNS   
 const nextBtn = document.getElementById("nextBtn");
-const prevBtn = document.getElementById("prevBtn")
-let test = document.getElementById("slider").style.backgroundImage = "url('My project6.jpg')";
-let counter = 0;
-
-nextBtn.addEventListener("click", () => {
-    if(test === "url('My project6.jpg')" && counter === 0){
-    document.getElementById("slider").style.backgroundImage = "url('My project3.jpg')";
-    document.getElementById("slider").style.transitionDuration = "1" + "s"
-    counter++
-    }else{
-        document.getElementById("slider").style.backgroundImage = "url('My project6.jpg')";
-        document.getElementById("slider").style.transitionDuration = "1" + "s"
-        counter--
-    }
-    
-});
-
-prevBtn.addEventListener("click", () => {
-    if(test === "url('My project6.jpg')" && counter === 0){
-        document.getElementById("slider").style.backgroundImage = "url('My project3.jpg')";
-        document.getElementById("slider").style.transitionDuration = "1" + "s"
+const prevBtn = document.getElementById("prevBtn");
+  //create counter and words value 
+let counter = 2;
+let words = ""
+  //making a loop on the next btn 
+for(let i = 1 ; i <= 4 ; i++){
+    nextBtn.onclick = function(){
+        if(counter === 2){
         counter++
+        words = `url('My project${counter}.jpg')`
+        document.getElementById("slider").style.backgroundImage = words
+        document.getElementById("slider").style.transitionDuration = "1" + "s"
+        document.getElementById("slider").style.transitionDelay = "250" + "ms"
         }else{
-            document.getElementById("slider").style.backgroundImage = "url('My project6.jpg')";
+            words = `url('My project${counter + 1}.jpg')`
+            document.getElementById("slider").style.backgroundImage = words
             document.getElementById("slider").style.transitionDuration = "1" + "s"
-            counter--
+            document.getElementById("slider").style.transitionDelay = "250" + "ms"
+            counter++
         }
-});
+        if(counter > 5){
+            counter = 5;
+            words = `url('My project${counter}.jpg')`
+            document.getElementById("slider").style.backgroundImage = words
+            document.getElementById("slider").style.transitionDuration = "1" + "s"
+            document.getElementById("slider").style.transitionDelay = "250" + "ms"
+        }
+    }
+    // making loop on the prev btn
+    prevBtn.onclick = function(){
+        if(counter === 2){
+            words = `url('My project${counter}.jpg')`
+            document.getElementById("slider").style.backgroundImage = words
+            document.getElementById("slider").style.transitionDuration = "1" + "s"
+            document.getElementById("slider").style.transitionDelay = "250" + "ms"
+            console.log(counter)
+        }else{
+                words = `url('My project${counter - 1}.jpg')`
+                console.log(words)
+                document.getElementById("slider").style.backgroundImage = words
+                document.getElementById("slider").style.transitionDuration = "1" + "s"
+                document.getElementById("slider").style.transitionDelay = "250" + "ms"
+                counter--
+                console.log(counter)
+            }
+    }
+}
+// end of slider
+
